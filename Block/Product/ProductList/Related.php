f5c41db753e406c9a280842850dc2ac52206b77f
Bruno Dias <bruno@outeredgeuk.com>
2016-10-31 10:43:51 +0000

    Changed item colletion

--- Block/Product/ProductList/Related.php
+++ Block/Product/ProductList/Related.php
@@ -22,20 +22,13 @@ class Related extends \Magento\Catalog\Block\Product\ProductList\Related
         )->setPositionOrder()->addStoreFilter();
 
         if ($this->moduleManager->isEnabled('Magento_Checkout')) {
-            //$this->_addProductAttributesAndPrices($this->_itemCollection);
+            $this->_addProductAttributesAndPrices($this->_itemCollection);
         }
-
+      
         $this->_itemCollection->load();
 
         foreach ($this->_itemCollection as $product) {
             $product->setDoNotUseCategoryId(true);
-            if ($product->getLinkId() && $product->getPrice()){
-                $product->setTierPrice($product->getPrice());
-                $product->setMinimalPrice($product->getPrice());
-                $product->setMinPrice($product->getPrice());
-                $product->setFinalPrice($product->getPrice());
-                $product->setMaxPrice($product->getPrice());
-            }
         }
 
         return $this;
